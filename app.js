bd3c5e317dd72503fd6429fa483a29829931d150
mahmoud-123-jjj <krash1120@gmail.com>
2021-01-27 16:05:22 +0200

    a

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json());
 
 
 app.use((req, res, next) => {
-    // res.setHeaders('Access-Control-Allow-Origin', '*');
-    // res.setHeaders('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
-    // res.setHeaders('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeaders('Access-Control-Allow-Origin', '*');
+    res.setHeaders('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
+    res.setHeaders('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
