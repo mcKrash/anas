9ffe728b96c45779ad06c8e854152eba2ba12f23
mahmoud-123-jjj <krash1120@gmail.com>
2021-01-27 19:15:48 +0200

    first

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/users'); 
 
 const app = express();
-const port = (process.env.PORT);
+const port = (process.env.PORT || 5000);
 
 app.use(bodyParser.json());
 
 
-app.use((req, res, next) => {
-    res.setHeaders('Access-Control-Allow-Origin', '*');
-    res.setHeaders('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
-    res.setHeaders('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeaders('Access-Control-Allow-Origin', '*');
+//     res.setHeaders('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
+//     res.setHeaders('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 const mongoUrl = 'mongodb+srv://proengan:proengan@cluster0.hsjtr.mongodb.net/test?retryWrites=true&w=majority&ssl=true';
 const mongooseOptions = {
